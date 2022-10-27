dfeaea5471fb03a7c5a834d0a0a3eace3392c674
Priyal <priyalstella2002@gmail.com>
2022-10-26 21:14:45 -0400

    trial

---
+++ src/pages/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+import "./App.css"
+import "@fontsource/itim" 
+
+const logo = require('../images/logo.png')
+const Navbar= () =>{
+  return (
+  <div className='navigation'>
+    <div className='navBox'>
+      <img src={logo} className="logo"></img>
+    <Link style={{textDecoration:'none'}} to="/" ><h1 id="appName">Give <span id="good"> Good</span> </h1></Link>
+     <Link style={{textDecoration:'none'}} to="/createAccount" id="signup"><button id="signupText">Sign up</button></Link>
+
+    
+
+    </div>
+    
+  </div>
+  );
+}
+export default Navbar;
