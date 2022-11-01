6df64bd4e2381817e80c2581f36e7a51221e2fec
Priyal <priyalstella2002@gmail.com>
2022-10-31 20:54:16 -0400

    separated css files

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
-import "./navbar.css"
 import "@fontsource/itim" 
+import "./navbar.css"
 
 const logo = require('../images/logo.png')
 const Navbar= () =>{
